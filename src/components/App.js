1f12bc320502168daef4619c4f7d322da7752f93
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-04-13 00:53:53 +0530

    Initial Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,56 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [fields, setFields] = useState([{ name: "", age: "" }]);
 
-export default App
+  const handleChange = (inx, e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    const newFields = [...fields];
+    newFields[inx][name] = value;
+    setFields(newFields);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+    console.log(fields);
+  };
+  const handleAddField = () => {
+    setFields([...fields, { name: "", age: "" }]);
+  };
+  const handleRemove = (inx) => {
+    const newFields = [...fields];
+    newFields.splice(inx, 1);
+    setFields(newFields);
+  };
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        {fields.map((f, inx) => (
+          <div key={inx}>
+            <input
+              type="text"
+              name="name"
+              value={f.name}
+              onChange={(e) => handleChange(inx, e)}
+              placeholder="Name"
+            />
+            <input
+              type="text"
+              name="age"
+              value={f.age}
+              onChange={(e) => handleChange(inx, e)}
+              placeholder="Age"
+            />
+            <button onClick={() => handleRemove(inx)}>Remove</button>
+          </div>
+        ))}
+        <button onClick={handleAddField}>Add more...</button>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
